5d67870e3f4a769bcc771806a68e5daee9572f30
Amit Gupta <137424796+Amit-S-G@users.noreply.github.com>
2024-04-22 13:03:43 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from queue import PriorityQueue
 from geopy.geocoders import Nominatim
 from math import inf
 
-city_bbox = 29.7241, 29.5801, -82.2558, -82.4289
 
 # Initializing the walking network!
-G = ox.graph_from_place("Gainesville, Florida", network_type="walk")
+G = ox.graph_from_place("Paris, France", network_type="walk")
 ox.convert.to_undirected(G)
 
 def check_address(address, G):
@@ -17,9 +16,9 @@ def check_address(address, G):
     location = geoGuesser.geocode(address)
     # If this was successful; that is, if the address is real and can be converted into coordinates
     if location:
-        # Check to see that the coordinates are within the city's general bounding box.
-        # This *might* change to be, like, France in general, because the bounding boxes for cities are tough to get down.
-        if 29.7241 > location.latitude > 29.5801 and -82.2558 > location.longitude > -82.4289:
+        # Check to see that the coordinates are within the city's general bounding box
+        # City bounding box based on online resources
+        if 48.812103 < location.latitude < 48.904695 and 2.246475 < location.longitude < 2.422256:
             # If the location is in bounds, find the node nearest it and return it
             node = ox.distance.nearest_nodes(G, location.longitude, location.latitude)
             return node
@@ -29,6 +28,50 @@ def check_address(address, G):
     # If the conversion to coordinates did not work, return nothing
     else:
         return None
+
+def dfs_shortest_path(G, startNode_id, endNode_id):
+    # Initialize a stack for DFS
+    toVisit = []
+    # Keep track of visited nodes
+    visited = set()
+    # Empty dict of nodes and their parents
+    parents = {}
+    toVisit.append(startNode_id)
+    visited.add(startNode_id)
+
+    # DFS algorithm
+    while toVisit: # Continues loop until the stack is empty
+        # Let's get the current node's ID
+        currNode = toVisit.pop()
+        # If this is the node we want, break out of DFS
+        if currNode == endNode_id:
+            break
+        # Else explore neighbors of the current node
+        for neighbor in G.neighbors(currNode):
+            # Checks if the neighbor has not been visited
+            if neighbor not in visited:
+                # Grab the information about the edge between currNode and neighbors
+                edge_data = G.get_edge_data(currNode, neighbor)
+                single_edge_data = list(edge_data.values())[0]
+                edge_id = single_edge_data['osmid']
+                edge_length = single_edge_data['length']
+                toVisit.append(neighbor)
+                visited.add(neighbor)
+                parents[neighbor] = (currNode, edge_id, edge_length)
+
+    # If we did the traversal and never reached the endNode, return None
+    if currNode != endNode_id:
+        return None
+    # If we did find it
+    else:
+        total_length = 0
+        # Iterate through the parents dict to retrace the path
+        while currNode != startNode_id:
+            # Add the length of the edge used to reach currNode
+            total_length += parents[currNode][2]
+            currNode = parents[currNode][0]
+        return total_length, parents
+
 
 # Define Breadth-First Search function
 def bfs_shortest_path(G, startNode_id, endNode_id):
@@ -129,36 +172,51 @@ def a_star_search(startNode_id, endNode_id, G):
     return totalWeight, parents
 
 def main():
-    address = input("Enter a starting address: ")
-    startNode = check_address(address, G)
-    if startNode:
-        print("Sounds good!")
-    else:
-        print("Sorry, that address either doesn't exist, or it isn't in Gainesville. Rerun!")
-        sys.exit()
+    while True:
+        address = input("Enter a starting address: ")
+        startNode = check_address(address, G)
+        if startNode:
+            print("Sounds good!")
+            break
+        else:
+            print("Sorry, that address either doesn't exist, or it isn't in Paris. Rerun!")
+    while True:
+        address = input("Enter a destination address: ")
+        endNode = check_address(address, G)
+        if endNode:
+            print("Sounds good!")
+            break
+        else:
+            print("Sorry, that address either doesn't exist, or it isn't in Gainesville. Rerun!")
 
-    address = input("Enter a destination address: ")
-    endNode = check_address(address, G)
-    if endNode:
-        print("Sounds good!")
-    else:
-        print("Sorry, that address either doesn't exist, or it isn't in Gainesville. Rerun!")
-        sys.exit()
+    # Draw the graph for them, blank!
+    node_sizes = {startNode: 15, endNode: 15}
+
+
+    # Set size 0 for all other nodes
+    for node in G.nodes():
+        if node not in node_sizes:
+            node_sizes[node] = 0
+
+    ox.plot_graph(G, node_size=[node_sizes[node] for node in G.nodes()])
+    print("Here is a blank map depicting your start and end locations.")
 
     # Ask the user to choose the algorithm
-    algorithm = input("Choose an algorithm (A* or BFS): ").upper()
+    algorithm = input("Choose an algorithm (A*, BFS, or DFS): ").upper()
 
     if algorithm == "A*":
         shortest_path_result = a_star_search(startNode, endNode, G)
     elif algorithm == "BFS":
         shortest_path_result = bfs_shortest_path(G, startNode, endNode)
+    elif algorithm == "DFS":
+        shortest_path_result = dfs_shortest_path(G, startNode, endNode)
     else:
-        print("Invalid algorithm choice. Please choose either A* or BFS.")
+        print("Invalid algorithm choice. Please choose either A*, BFS, or DFS.")
         sys.exit()
 
     if shortest_path_result:
         shortest_path_length, shortest_path_parents = shortest_path_result
-        print("Shortest path length:", shortest_path_length)
+        print(f"Shortest path length: {shortest_path_length:.2f} meters.")
 
         # Extract the shortest path from the parents dictionary
         shortest_path = []
@@ -169,17 +227,17 @@ def main():
         shortest_path.append(startNode)
         shortest_path.reverse()
 
-        print("Would you like to see nodes visited or shortest route (nodes visited or shortest route): ", end="")
+        print("Would you like to see nodes visited or route taken (nodes visited or route taken): ", end="")
         user_input = input().strip()
 
-        if user_input == "shortest route":
+        if user_input == "route taken":
             # Plot the graph with colored nodes and the shortest path
             node_sizes = {startNode: 15, endNode: 15}
             for node in G.nodes():
                 if node not in node_sizes:
                     node_sizes[node] = 0
 
-            ox.plot.plot_graph_route(G, node_size=[node_sizes[node] for node in G.nodes()], route=shortest_path, show=False, save=True, filepath='street_map.png')
+            ox.plot.plot_graph_route(G, node_size=[node_sizes[node] for node in G.nodes()], route=shortest_path, show=True, save=True, filepath='street_map.png')
         elif user_input == "nodes visited":
             # Plot the graph with colored nodes, highlighting visited nodes in green and others in blue
             visited_nodes = set(shortest_path_parents.keys())
